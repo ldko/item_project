2517b46a2d23cc20d0fe92174653755aed2a01d0
Lauren Ko <laurendko@gmail.com>
2024-09-26 18:36:17 -0500

    Move toward pep8 conventions

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,11 @@ from item_app import views
 
 
 urlpatterns = [
-    ## main ---------------------------------------------------------
-    path( 'info/', views.info, name='info_url' ),
-    ## other --------------------------------------------------------
-    path( '', views.root, name='root_url' ),
-    path( 'admin/', admin.site.urls ),
-    path( 'error_check/', views.error_check, name='error_check_url' ),
-    path( 'version/', views.version, name='version_url' ),
+    # main ---------------------------------------------------------
+    path('info/', views.info, name='info_url'),
+    # other --------------------------------------------------------
+    path('', views.root, name='root_url'),
+    path('admin/', admin.site.urls),
+    path('error_check/', views.error_check, name='error_check_url'),
+    path('version/', views.version, name='version_url'),
 ]
