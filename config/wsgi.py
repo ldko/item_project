366328d5302d0a73f048a2dcc14d31b06eb628cc
Lauren Ko <laurendko@gmail.com>
2024-09-26 18:37:02 -0500

    Move wsgi.py toward pep8 conventions

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,14 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os, pathlib, sys
+import os
+import pathlib
+import sys
 from django.core.wsgi import get_wsgi_application
 
 
 PROJECT_DIR_PATH = pathlib.Path(__file__).resolve().parent.parent
-# print( f'PROJECT_DIR_PATH, ``{PROJECT_DIR_PATH}``' )
+# print(f'PROJECT_DIR_PATH, ``{PROJECT_DIR_PATH}``')
 
-sys.path.append( str(PROJECT_DIR_PATH) )
+sys.path.append(str(PROJECT_DIR_PATH))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'  # so django can access its settings
 
